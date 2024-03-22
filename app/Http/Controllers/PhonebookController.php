c3d6020eb82fc0e64093eac2b86a52e684b5c707
Igor C. de Paula <principe.borodin@gmail.com>
2024-03-22 10:49:52 -0300

    fix: add report phonebook

--- app/Http/Controllers/PhonebookController.php
+++ app/Http/Controllers/PhonebookController.php
@@ -6,6 +6,7 @@ use App\Http\Requests\StorePhonebookRequest;
 use App\Http\Requests\UpdatePhonebookRequest;
 use App\Models\Phonebook;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 
 class PhonebookController extends Controller
 {
@@ -32,6 +33,7 @@ class PhonebookController extends Controller
     {
         return new JsonResponse(Phonebook::create($request->all()), JsonResponse::HTTP_CREATED);
     }
+
     /**
      * Update the specified resource in storage.
      */
@@ -46,5 +48,15 @@ class PhonebookController extends Controller
     public function destroy(Phonebook $phonebook)
     {
         return new JsonResponse($phonebook->delete(), JsonResponse::HTTP_ACCEPTED);
+    }
+
+    public function report(Request $request)
+    {
+        $phone = new Phonebook();
+        if ($request->name) {
+            $phones = $phone->where('name', 'like', '%' . $request->name . '%')->get();
+            return view('report', ['data' => $phones]);
+        }
+        return view('report', ['data' => $phone->all()]);
     }
 }
